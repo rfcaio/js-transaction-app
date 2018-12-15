ed67795d8828a8d4e3ae97cd875a915968235832
Caio Rodrigues <rfcaio90@gmail.com>
2018-12-15 13:43:58 -0300

    jsa1-aula-02

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css'
+
+import TransactionModel from './models/TransactionModel'
+
+let t = new TransactionModel(new Date('2015-12-8'), 4, 150)
+
+console.log(t.date)
---
+++ src/models/TransactionModel.js
@@ -0,0 +1,28 @@
+
+class TransactionModel {
+  constructor (date, amount, value) {
+    this._date = new Date(date.getTime())
+    this._amount = amount
+    this._value = value
+
+    Object.freeze(this)
+  }
+
+  get date () {
+    return new Date(this._date.getTime())
+  }
+
+  get amount () {
+    return this._amount
+  }
+
+  get value () {
+    return this._value
+  }
+
+  get volume () {
+    return this._amount * this._value
+  }
+}
+
+export default TransactionModel
